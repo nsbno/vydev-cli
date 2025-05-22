nsbno/vydev-cli

commit 577473a47541f6e5d711f43d00e7c7a410147c00

Fix users that use granted could not use the tool
Granted has the account ID in a separate value

deployment_migration/infrastructure/parameter_store.py
@@ -57,14 +57,21 @@ class AWSAWS(AWS):
         config.read(config_path)
         matching_profiles = []
 
+        possible_account_id_locations = [
+            "role_arn",
+            "credential_process",
+            "granted_sso_account_id",
+        ]
+
         for section in config.sections():
             if not section.startswith("profile "):
                 continue
 
             profile_name = section.replace("profile ", "")
 
-            if (account_id not in config[section].get("role_arn", "")) and (
-                account_id not in config[section].get("credential_process", "")
+            if not any(
+                account_id in config[section].get(location)
+                for location in possible_account_id_locations
             ):
                 continue
 

commit d8297977b7b8a6576193428db6a15f9227d88715

Fix bug introduced in 577473a4

deployment_migration/handlers/cli.py
@@ -37,6 +37,10 @@ from deployment_migration.infrastructure.application_context import (
 )
 
 
+class Terminal:
+    pass
+
+
 class CLIHandler:
     """
     Command-line interface handler for the deployment migration application.

deployment_migration/infrastructure/parameter_store.py
@@ -70,7 +70,7 @@ class AWSAWS(AWS):
             profile_name = section.replace("profile ", "")
 
             if not any(
-                account_id in config[section].get(location)
+                account_id not in config[section].get(location, "")
                 for location in possible_account_id_locations
             ):
                 continue